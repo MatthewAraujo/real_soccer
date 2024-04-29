d86c3b49178587f9763182eda2486a1d03660b0c
MatthewAraujo <matthewaraujo20@gmail.com>
2024-04-29 20:28:50 -0300

    feat

--- src/Components/firebase/firebase.jsx
+++ src/Components/firebase/firebase.jsx
@@ -1,15 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import dotenv from "dotenv";
-dotenv.config();
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBtvj0747A5QsRcoxqUluM9NXqIB4lRXJg",
+  authDomain: "futebol-3b89b.firebaseapp.com",
+  projectId: "futebol-3b89b",
+  storageBucket: "futebol-3b89b.appspot.com",
+  messagingSenderId: "8685824856",
+  appId: "1:8685824856:web:00acb1c62fed4ceb5b532d",
 };
 
 const app = initializeApp(firebaseConfig);
